bef63de5856dadec0b297c12a010b08b28b8ce6c
David Robert <contact.davidrobert@gmail.com>
2022-07-07 10:06:14 +0200

    repository find

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+
+    /**
+     * @Route("/insert-category", name="insert_category")
+     */
+    public function insertCategory(EntityManagerInterface $entityManager)
+    {
+        $category = new Category();
+
+        $category->setTitle("Ecologie");
+        $category->setColor("green");
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return new Response('OK');
+    }
+
+
+}
