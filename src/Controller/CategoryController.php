523fb6f55e64ee4434915351e54c68c31659be4a
David Robert <contact.davidrobert@gmail.com>
2022-07-07 15:37:19 +0200

    category repository

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -27,6 +28,30 @@ class CategoryController extends AbstractController
         $entityManager->flush();
 
         return new Response('OK');
+    }
+
+    /**
+     * @Route("/categories", name="list_categories")
+     */
+    public function listCategories(CategoryRepository $categoryRepository)
+    {
+       $categories = $categoryRepository->findAll();
+
+       return $this->render('list_categories.html.twig', [
+          'categories' => $categories
+       ]);
+    }
+
+    /**
+     * @Route("/categories/{id}", name="show_category")
+     */
+    public function showCategory($id, CategoryRepository $categoryRepository)
+    {
+        $category = $categoryRepository->find($id);
+
+        return $this->render('show_category.html.twig', [
+            'category' => $category
+        ]);
     }
 
 
